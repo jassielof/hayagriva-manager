2e40a30cd1b0e6f8e40316f2e599456da5cd475c
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-07-07 15:30:31 -0400

    cloudflare?

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,15 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({}),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
-  }
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
 
 export default config;
