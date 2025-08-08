04e99900594fa5bd156aa32716de6dc959880553
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-08-07 20:31:45 -0400

    formattable string inputs

--- src/routes/bibliography/[id]/entry/+page.svelte
+++ src/routes/bibliography/[id]/entry/+page.svelte
@@ -24,6 +24,7 @@
     <label for="entry-id" class="label">ID</label>
     <input
       id="entry-id"
+      placeholder="UMLAndPatterns"
       class="input w-full font-mono"
       type="text"
       bind:value={newEntryId}
@@ -35,6 +36,60 @@
       label="Title"
       placeholder="UML & Patterns"
       bind:value={newEntryData.title!}
+    />
+
+    <!-- TODO: Add Author input -->
+    <!-- TODO: Add Date input -->
+    <!-- TODO: Add ParentEntry input -->
+
+    <FormattableStringInput
+      label="Abstract"
+      placeholder="This article compares..."
+      bind:value={newEntryData.abstract!}
+      multiline={true}
+    />
+
+    <FormattableStringInput
+      label="Genre"
+      placeholder="Doctoral dissertation"
+      bind:value={newEntryData.genre!}
+    />
+
+    <FormattableStringInput
+      label="Call Number"
+      placeholder="QA76.76.D47 G66 2005"
+      bind:value={newEntryData['call-number']!}
+    />
+
+    <FormattableStringInput
+      label="Location"
+      placeholder="New York, NY"
+      bind:value={newEntryData.location!}
+    />
+
+    <FormattableStringInput
+      label="Organization"
+      placeholder="ACM"
+      bind:value={newEntryData.organization!}
+    />
+
+    <FormattableStringInput
+      label="Archive"
+      placeholder="Library of Congress"
+      bind:value={newEntryData.archive!}
+    />
+
+    <FormattableStringInput
+      label="Archive Location"
+      placeholder="Box 12, Folder 3"
+      bind:value={newEntryData['archive-location']!}
+    />
+
+    <FormattableStringInput
+      label="Note"
+      placeholder="See also related work in appendix."
+      bind:value={newEntryData.note!}
+      multiline={true}
     />
 
     <button class="btn btn-primary mt-4">Add</button>
