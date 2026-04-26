b1d62aebd9d9176aed4a298c02002509aa95a7d2
Jassiel Ovando <jassielovando@protonmail.com>
2026-04-26 18:10:05 -0400

    refactor: reorder imports and add global ignores in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -12,6 +12,7 @@ const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
+  globalIgnores(['json-schemas/']),
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs.recommended,
